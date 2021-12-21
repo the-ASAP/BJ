72240e12046964d55fc99852558bf6dfc24777f7
Makarenko Sergey <Rsmon1982@yandex.ru>
2021-12-21 13:55:27 +0300

    Markup header, card section adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 import * as $ from 'jquery';
 import '../scss/index.scss';
 import header from '../components/header.html';
+import card from '../components/card.html';
+import about from '../components/about.html';
+import offer from '../components/offer.html';
+import include from '../components/include.html';
+
 
 $(() => {
-  
   $('#root').prepend(header);
+  $('#root').append(card);
+  $('#root').append(about);
+  $('#root').append(offer);
+  $('#root').append(include);
 });
+
+
+
