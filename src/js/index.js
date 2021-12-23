a7a3489c76a1119c3f9ea6e2b09a92f5d33dcf57
Makarenko Sergey <Rsmon1982@yandex.ru>
2021-12-23 18:01:23 +0300

    End adaptive markup

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,8 @@ import recent from '../components/recent.html';
 import modal from '../components/modal.html';
 import footer from '../components/footer.html';
 import { doc } from 'prettier';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 
 
@@ -19,18 +21,123 @@ import { doc } from 'prettier';
 
 $(() => {
     // $('#root').prepend(header);
-    //   $('#root').prepend(header2);
-    $('#root').append(modal);
-    $('#root').append(card);
-    $('#root').append(about);
-    $('#root').append(offer);
-    $('#root').append(include);
+    // $('#root').append(modal);
+    // $('#root').append(card);
+    // $('#root').append(about);
+    // $('#root').append(offer);
+    // $('#root').append(include);
     $('#root').append(similar);
     $('#root').append(recent);
     //   $('#root').append(footer);
+
+    $('.selectboxss').selectbox();
+
+
+
+    // $("#zoom_03").ezPlus({
+    //     scrollZoom: true,
+    //     borderSize: 2,
+    //     gallery: 'gal1',
+    //     galleryActiveClass: 'active'
+    // });
+
+    $('#big-img').ezPlus({
+        zoomType: 'lens',
+    lensShape: 'round',
+    lensSize: 200,
+    scrollZoom: true,
+    gallery: 'gal1',
+    galleryActiveClass: 'active'
+    });
+
+
+
+    $('.owl-carousel-rec').owlCarousel({
+     
+        items: 3,
+        responsive:{
+            0:{
+              items:1
+            },
+            578:{
+              items:2
+            },
+            1180:{
+              items:3
+            }
+          }
+    });
+   
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        items: 4,
+        responsive:{
+            0:{
+              items:1
+            },
+            578:{
+              items:2
+            },
+            1180:{
+              items:3
+            }
+          }
+    });
+
+    
+    
+
+
+    let noSizeButton = document.querySelector('.product-detail__no-size--modal');
+    let modelCard = document.querySelector('.modal-card');
+    let modalCartCloseBtn = document.querySelector('.modal-card__close-btn');
+    let modelCardMobile = document.querySelector('.product-detail__no-size');
+    let btnBuy = document.querySelector('.product-detail__buy-btn');
+    let modalSalon = document.querySelector('.salon');
+    let modalWrapper = document.querySelector('.modal__wrapper');
+
+    let modalBtnClose = document.querySelector('.salon__close');
+
+    modalBtnClose.addEventListener('click', (e) => {
+
+        modalSalon.classList.remove('salon--show');
+        modalSalon.classList.add('salon--hide');
+        modalWrapper.classList.remove('active')
+    })
+
+    modalWrapper.addEventListener('click', (e) => {
+
+        modalSalon.classList.remove('salon--show');
+        modalSalon.classList.add('salon--hide');
+        modalWrapper.classList.remove('active')
+    })
+
+
+    noSizeButton.addEventListener('click', (e) => {
+        modelCard.classList.remove('modal-card--close');
+        modelCard.classList.add('modal-card--open');
+    })
+
+
+    modelCardMobile.addEventListener('click', (e) => {
+
+        modelCard.classList.remove('modal-card--close');
+        modelCard.classList.add('modal-card--open');
+    })
+
+
+    modalCartCloseBtn.addEventListener('click', (e) => {
+        modelCard.classList.remove('modal-card--open');
+        modelCard.classList.add('modal-card--close');
+    })
+
+    btnBuy.addEventListener('click', (e) => {
+        modalSalon.classList.remove('salon--hide');
+        modalSalon.classList.add('salon--show');
+        modalWrapper.classList.add('active')
+    })
+
 });
-
-
 
 
 $.fn.selectbox = function () {
@@ -53,18 +160,4 @@ $.fn.selectbox = function () {
     });
 };
 
-
-
-
-import './../vendors/jquery.14.js';
-
-
-setTimeout(() =>  $("#zoom_01").ezPlus(), 2000)
-
-$(document).ready(function () { $('.selectboxss').selectbox(); });
-$(document).ready(function () { $("#zoom_01").ezPlus()});
-
-
-
-//***************************** */
 
